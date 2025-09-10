f3f57a5235727a5ea8ac6f832e864fab60e4b673
Hamed Pour <hamed_k.pour@hotmail.com>
2025-09-10 19:44:21 +0100

    adding end of session content

--- Foundation/03_If_else_Conditionals/hameds_notes/01_condition.py
+++ Foundation/03_If_else_Conditionals/hameds_notes/01_condition.py
@@ -5,3 +5,17 @@ Booleans (bool) in Python have two values: True and False
 Comparison operators create boolean results
 Logical operators (and, or, not) combine boolean values
 """
+
+
+temp = 34
+
+is_sunny = False
+am_feeling_like_getting_wet = True
+
+
+go_to_beach = temp > 28 and is_sunny or am_feeling_like_getting_wet
+
+
+print("am I going to the beach?", go_to_beach)
+
+
