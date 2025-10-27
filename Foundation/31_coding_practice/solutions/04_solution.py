76c8359df5cca7aa6843870d9af0b4be3237613e
Hamed Pour <hamed_k.pour@hotmail.com>
2025-10-27 20:53:26 +0000

    session 31 end

--- Foundation/31_coding_practice/solutions/04_solution.py
+++ Foundation/31_coding_practice/solutions/04_solution.py
@@ -1,3 +1,17 @@
+"""
+Note from Hamed - BOTH solutions below are correct. HOWEVER, the one that uses "set" is more
+optimised and works better/faster for large inputs of data. That's the only difference.
+"""
+
+def remove_duplicates(s):
+    result = []
+    for char in s:
+        if char not in result:
+            result.append(char)
+
+    return ''.join(result)
+
+
 def remove_duplicates(s):
     seen = set()
     result = []
